b3075ae1595d4f3d430d0c4644cdc1d04ed77db8
Albert Dominguez <albertdominguez@ucsb.edu>
2020-08-16 12:44:16 -0700

    Exercise8

---
+++ R/day-08.R
@@ -0,0 +1,19 @@
+# Albert Dominguez
+# August 16, 2020
+
+state.of.interest = "Oregon"
+covid %>%
+  filter(state == state.of.interest) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(newCases = cases - lag(cases),
+         roll7 = rollmean(newCases, 7, fill = NA, align="right")) %>%
+  ggplot(aes(x = date)) +
+  geom_col(aes(y = newCases), col = NA, fill = "#F5B8B5") +
+  geom_line(aes(y = roll7), col = "darkred", size = 1) +
+  ggthemes::theme_wsj() +
+  labs(title = paste("New Reported cases by day in", state.of.interest)) +
+  theme(plot.background = element_rect(fill = "white"),
+        panel.background = element_rect(fill = "white"),
+        plot.title = element_text(size = 14, face = 'bold')) +
+  theme(aspect.ratio = .5)
